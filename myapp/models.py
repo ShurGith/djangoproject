27742abb2c01c59672c88b4bf0f152ce0774376d
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-09-06 16:21:08 +0200

    Artes

--- myapp/models.py
+++ myapp/models.py
@@ -5,12 +5,14 @@ class Project(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='task')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.title + ' - ' + self.project.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import Project, Task
 
 # Create your views here.
 def blog(request):
-    return render(request,'blog.html')
+    projects = Project.objects.all()
+    return render(request,'blog.html',{
+    'projects':projects
+    })
 
 def about(request):
     return render(request,'about.html')
